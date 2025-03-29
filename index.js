65428393efa380792ea06fdfbd8ba14887b2912f
mytopgraf <mytopgraf@gmail.com>
2025-03-29 20:22:11 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,19 +1,5 @@
-const fetch = require("node-fetch");
+// index.js
 
 module.exports = async function (req, res) {
-  try {
-    const { message } = JSON.parse(req.body); // Получаем текст заявки
-
-    const botToken = process.env.BOT_TOKEN;
-    const chatId = process.env.CHAT_ID;
-    
-    const url = `https://api.telegram.org/bot${botToken}/sendMessage?chat_id=${chatId}&text=${encodeURIComponent(message)}`;
-
-    const response = await fetch(url);
-    const data = await response.json();
-
-    return res.json({ success: true, data });
-  } catch (error) {
-    return res.json({ success: false, error: error.message });
-  }
+    return res.json({ message: "Hello, World!" });
 };
